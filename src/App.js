c1dcc60d33f8e97662e3a43577319d1d54073f11
snipergn <andrei.banita@gmail.com>
2023-02-13 22:54:48 +0200

    new commit

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('https://smart-brain-api-kyzp.onrender.com/imageurl', {
+      fetch('https://smart-brain-1tnp.onrender.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -75,7 +75,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://smart-brain-api-kyzp.onrender.com/image', {
+          fetch('https://smart-brain-1tnp.onrender.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://smart-brain-api-kyzp.onrender.com/register', {
+    fetch('https://smart-brain-1tnp.onrender.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
